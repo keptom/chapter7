be408c81092014c1b3496344b0183ce075173c2a
bob <bob>
2017-10-27 15:01:38 +0200

    Adding new modules.

---
+++ NewPythonProject/src/trojam.py
@@ -0,0 +1,9 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__ = "bob"
+__date__ = "$Oct 27, 2017 2:51:04 PM$"
+
+if __name__ == "__main__":
+    print "Hello World"
