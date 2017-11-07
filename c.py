7eac751e15be10161e2f37908b7f91a12fd37ac4
keptom <33152167+keptom@users.noreply.github.com>
2017-11-07 03:24:17 -0800

    Update c.py

--- c.py
+++ c.py
@@ -56,7 +56,7 @@ def mail():
         mailServer.login(gmail_user, gmail_pwd)
         mailServer.sendmail(gmail_user,to, msg.as_string())
         mailServer.close()
-
+        return str(main)
     
 
 def main():
